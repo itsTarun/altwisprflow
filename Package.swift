48916e25236ce775da8f182270761f5725a7d877
Tarun <itstarun1994@gmail.com>
2026-03-06 21:42:41 +0530

    chore: add mlx-swift dependencies

--- Package.swift
+++ Package.swift
@@ -11,17 +11,21 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "OpenAI", url: "https://github.com/MacPaw/OpenAI.git", from: "0.2.5"),
-        .package(name: "GRDB", url: "https://github.com/groue/GRDB.swift.git", from: "6.0.0"),
-        .package(name: "KeychainAccess", url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2")
+        .package(url: "https://github.com/MacPaw/OpenAI.git", from: "0.2.5"),
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.0.0"),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
+        .package(url: "https://github.com/ml-explore/mlx-swift.git", from: "0.30.0"),
+        .package(url: "https://github.com/Blaizzy/mlx-audio-swift.git", from: "0.1.0")
     ],
     targets: [
         .executableTarget(
             name: "AltWisprFlow",
             dependencies: [
                 .product(name: "OpenAI", package: "OpenAI"),
-                .product(name: "GRDB", package: "GRDB"),
-                .product(name: "KeychainAccess", package: "KeychainAccess")
+                .product(name: "GRDB", package: "GRDB.swift"),
+                .product(name: "KeychainAccess", package: "KeychainAccess"),
+                .product(name: "MLX", package: "mlx-swift"),
+                .product(name: "MLXAudio", package: "mlx-audio-swift")
             ],
             path: "Sources/AltWisprFlow"
         )
