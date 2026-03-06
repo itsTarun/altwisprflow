f51966ca12c04cf96f3e6faa84f1c7c938e2853f
Tarun <itstarun1994@gmail.com>
2026-03-06 21:46:31 +0530

    feat: implement ModelDownloader for MLX Whisper weights

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "AltWisprFlow",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     products: [
         .executable(
             name: "AltWisprFlow",
